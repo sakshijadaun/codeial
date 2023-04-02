dd319a8222f53747efc4fc10d303e9ad81843bcb
Sakshi Jadaun <sakshijadaun080@gmail.com>
2023-04-01 18:23:03 -0700

    Render home file

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,7 @@
 //funtion publically available to the routes which returns something
 module.exports.home=function(req,res){
-    return res.end('<h1>Express is up for codeial</h1>');
+    //return res.end('<h1>Express is up for codeial</h1>');
+    return res.render('home',{
+        title: "Home"
+    });
 }
